a25fc63ddb703f857824242f2a2cde1ee086fbe7
Bonny-code <ndiboniface@outlook.com>
2021-06-25 15:30:07 +0100

    first commit

---
+++ D5-extra.js
@@ -0,0 +1,41 @@
+/* EXERCISE 11
+Write a function "checkArray" which receives an array of random numbers (created with giveMeRandom) and prints, for each item, whether it's bigger than 5.
+The function returns the sum of the numbers bigger than 5.
+*/
+
+/* WRITE YOUR CODE HERE */
+
+/* EXERCISE 12
+In your eCommerce you have an array of objects called shoppingCart. In this array you have a number of objects with a price, a name, an id and the quantity to be shipped.
+Create a function "shippingCartTotal" which calculates the total due to the shop.
+*/
+
+/* WRITE YOUR CODE HERE */
+
+/* EXERCISE 13
+In your eCommerce you have an array of objects called shoppingCart. In this array you have a number of objects with a price, a name, an id and the quantity to be shipped.
+Create a function "addToShoppingCart" which receives a new object, adds it to shoppingCart and returns the total number of items in the shoppingCart.
+*/
+
+/* WRITE YOUR CODE HERE */
+
+/* EXERCISE 14
+In your eCommerce you have an array of objects called shoppingCart. In this array you have a number of objects with a price, a name, an id and the quantity to be shipped.
+Create a function "maxShoppingCart" which receives the shoppingCart array and returns the most expensive item in the array.
+*/
+
+/* WRITE YOUR CODE HERE */
+
+/* EXERCISE 15
+In your eCommerce you have an array of objects called shoppingCart. In this array you have a number of objects with a price, a name, an id and the quantity to be shipped.
+Create a function "latestShoppingCart" which receives the shoppingCart array and returns the last item.
+*/
+
+/* WRITE YOUR CODE HERE */
+
+/* EXERCISE 16
+Create a function "loopUntil" which receives an integer x between 0 and 9.
+The function loops and prints a random number between 0 and 9 until the random number is bigger than x for three times in a row.
+*/
+
+/* WRITE YOUR CODE HERE */
